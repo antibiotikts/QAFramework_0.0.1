54925ad34de7f517f25d518a23d9986eaea1d73c
antibiotikts <107547014+antibiotikts@users.noreply.github.com>
2023-08-17 17:40:58 +0300

    Set up build.gradle for starting tests with used testng.xml

--- src/test/java/demoqa/positive_tests/BaseTest.java
+++ src/test/java/demoqa/positive_tests/BaseTest.java
@@ -1,15 +1,22 @@
 package demoqa.positive_tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeSuite;
 
 import static com.codeborne.selenide.Configuration.*;
 
 public class BaseTest {
+	private static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+
 	@BeforeSuite
 	public static void setUp() {
 		browserSize = "1920x1080";
 		timeout = 4000;
 		browser = "firefox";
-		baseUrl = "https://demoqa.com/";
+
+		logger.info("Set Up: browser size- " + browserSize);
+		logger.info("Set Up: timeout- " + timeout);
+		logger.info("Set Up: browser- " + browser);
 	}
 }
--- src/test/java/demoqa/positive_tests/HomePageTest.java
+++ src/test/java/demoqa/positive_tests/HomePageTest.java
@@ -1,8 +1,10 @@
 package demoqa.positive_tests;
 
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import projects.demoqa.pages.HomePage;
@@ -11,12 +13,16 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class HomePageTest extends BaseTest {
 
+	private static final String URL = "https://demoqa.com/";
+
+	private static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+
 	private final HomePage homePage = new HomePage();
 
-	@BeforeTest
+	@BeforeClass
 	public void openSite() {
-		System.out.println("open site");
-		open("https://demoqa.com/");
+		open(URL);
+		logger.info(URL + "is opened");
 	}
 
 	@DataProvider(name = "data")
@@ -32,7 +38,7 @@ public class HomePageTest extends BaseTest {
 	public void testing(String name) {
 
 		homePage.openCard(name);
-		System.out.println($(By.className("main-header")).getText());
+		logger.info("test param: " + name);
 		Assert.assertEquals(name, $(By.className("main-header")).getText());
 		back();
 	}
